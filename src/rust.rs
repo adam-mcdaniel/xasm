dbefb8f5186e2b9efe1de67766e39fa4c031d341
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-10-02 14:29:49 -0400

    Added new xmachine version

--- src/rust.rs
+++ src/rust.rs
@@ -14,7 +14,7 @@ edition = "2018"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-xmachine = 0.2.1
+xmachine = "0.2.1"
 "#;
 
 impl Compile for Rust {
